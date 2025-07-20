6a2f32bb4af1705cb9a83982f207020eb00262ba
OnkarJondhale <onkar.jondhale@gmail.com>
2025-07-20 17:13:54 +0530

    Updated dist file

--- frontend/dist/assets/latency-chart-Bj5OSYzg.es-9qI-W8-O.js
+++ frontend/dist/assets/latency-chart-Bj5OSYzg.es-9qI-W8-O.js
@@ -1,4 +1,4 @@
-import{r as W,R as ti,j as Ue}from"./index-Bmnl64te.js";/*!
+import{r as W,R as ti,j as Ue}from"./index-CPs79zEa.js";/*!
  * @kurkle/color v0.3.4
  * https://github.com/kurkle/color#readme
  * (c) 2024 Jukka Kurkela
