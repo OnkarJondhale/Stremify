f8ca298ec270289e02fa27c304a744ba8934926b
OnkarJondhale <onkar.jondhale@gmail.com>
2025-08-04 19:24:16 +0530

    Fix cookie and token issue

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use("/api/user",userRouter);
 app.use("/api/chat",chatRouter);
 
 app.get("/",(req,res)=>{
-    res.send("Hello,World!");
+    res.send("Hello,World!->Fix cookies");
 })
 
 app.listen(PORT,()=>{
